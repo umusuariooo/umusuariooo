ba974a1023d72b3fa18b38a3004c874ada0348bb
umusuariooo <mbv2004bo@gmail.com>
2024-03-02 08:17:45 -0300

    Maquina online

---
+++ cliente/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def cadastro(request):
+    return render(request, 'cadastro.html')
